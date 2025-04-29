bfe862fb1ef7da8c0db54a30fc4858e370f8bdbf
parthait2003 <customer@techpenta.com>
2025-04-30 00:30:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    webpack: (config) => {
+        // Disable Webpack cache for troubleshooting serialization warnings
+        config.cache = false;
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
